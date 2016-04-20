5afc6b27880c6f3718d5ec973d4f17bb64592ad7
Jon Erik Suero <joneriks@nulogy.com>
2016-04-20 06:06:45 -0400

    improves rspec tests

--- spec/benchmark_presenter_spec.rb
+++ spec/benchmark_presenter_spec.rb
@@ -14,9 +14,9 @@ describe BenchmarkPresenter do
       })
 
       expect { benchmark_presenter.run }.to output(/MY TITLE/).to_stdout
-      expect { benchmark_presenter.run }.to output(/a = 0.5/).to_stdout
+      expect { benchmark_presenter.run }.to output(/a \= 0.5/).to_stdout
       expect { benchmark_presenter.run }.to output(/a \+ 10/).to_stdout
-      expect { benchmark_presenter.run }.to output(/10.5/).to_stdout
+      expect { benchmark_presenter.run }.to output(/10\.5/).to_stdout
     end
 
     it 'should present exception messages' do
@@ -31,6 +31,7 @@ describe BenchmarkPresenter do
 
       expect(a).to receive(:fail).at_least(:once).and_raise("My Exception Message")
       expect { benchmark_presenter.run }.to output(/MY TITLE/).to_stdout
+      expect { benchmark_presenter.run }.to output(/a.fail/).to_stdout
       expect { benchmark_presenter.run }.to output(/My Exception Message/).to_stdout
     end
   end
